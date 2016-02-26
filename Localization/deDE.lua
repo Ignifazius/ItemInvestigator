1ec32430a690678a82914e9eebcf67e237d1fefa
Stefan Hobohm <s.hobohm@tu-bs.de>
2016-02-26 16:41:43 +0100

    adding german localization file

---
+++ Localization/deDE.lua
@@ -0,0 +1,35 @@
+ItemAnalysis_AddLocale("deDE", {
+	["StartAnalysis"] = "Item Analysis: Spieler auswählen um analyse zu starten",
+	["ConfigSpecLabel"] = "Alle gescannten Spezialisierungen speichern",
+	["ConfigSpecLabelTooltip"] = "Alle Spezialisierungen speichern, die ein gescanntes Ziel bisher hatte",
+	["ConfigRaidsLabel"] = "Analysiere Schlachtzugsforschritt",
+   	["ConfigRaidsLabelTooltip"] = "Dies könnte einen kurzes Standbild verursachen, wenn ein Ziel analysiert wird. Ist im Kampf automatisch deaktiviert.",
+   	["ConfigRaidsInCombatLabel"] = "Analysiere Schlachtzugsforschritt, auch während eines Kampfes.",
+    ["ConfigRaidsInCombatLabelTooltip"] = "",
+    ["ConfigGuildLabel"] = "Analysiere Gildenrank",
+    ["ConfigGuildLabelTooltip"] = "",
+	["ScannedMinutesAgo"] = "Vor %i Minuten gescannt",
+	["ScannedHoursAgo"] = "Vor %i Stunden gescannt",
+	["Rank"] = "Rang: %s (%s)",
+	["Spec"] = "Spec: %s",
+	["AvgIlvl"] = "Avg ilvl: %i", --just leave it "avg", the correct term would be "durchschn."
+	["ProgressHM"] = "HF: %s",
+	["ProgressBRF"] = "SFS: %s",
+	["ProgressHFC"] = "HFZ: %s",
+	["RaidNormal"] = "N",
+	["RaidHeroic"] = "H",
+	["RaidMythic"] = "M",
+	["Curve"] = "C",
+	["Edge"] = "E",
+	["NA"] = "Nicht verfügbar",
+	["HasUpgrades"] = "Hat %i/%i Gegenständen aufgerüstet",
+	["IncompleteScan"] = "**Unvollständiger Scan (oder fehlende Gegenstände)**",
+	["MissingEnchants"] = "%i fehlende Verzauberungen",
+	["MissingGems"] = "%i fehlende Edelsteine",
+	["HasPVPItems"] = "Hat %i PvP Gegenstände", -- even in German PvP is way more often used than SvS
+	["StatsFromGear"] = "Hat %i %s durch Ausrüstungsgegenstände",
+	["NotHitCapped"] = "Nicht an der Trefferlimit", --deprecated?
+	["NotExpCapped"] = "Nicht am Erfahrungslimit", -- not 100% sure, context missing
+	["NoArmorSpecialization"] = "Kein Bonus durch Rüstungsspezialisierung",
+	["WrongGear"] = "Hat Ausrüstung mit %s"
+	});
